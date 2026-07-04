a56d8afa2524b8194c7c4813d0098d4d897ee688
DYRONIX-BEAST\daved <davedebreuck.gamedeveloper@gmail.com>
2026-07-04 13:53:01 +0200

    build: add -game= option to run.bat for launching non-default games
    Now that source/games can hold more than one game, run.bat needs a way to
    pick which built exe to launch instead of always assuming dyro_game.

--- run.bat
+++ run.bat
@@ -3,32 +3,59 @@ setlocal
 
 set "CONFIG=Debug"
 set "CONFIG_ARG=-debug"
+set "GAME=dyro_game"
 
-if not "%~2"=="" (
-    echo Usage: %~nx0 [-debug^|-release]
-    exit /b 1
-)
+:parse_args
+if "%~1"=="" goto args_done
 
 if /i "%~1"=="-debug" (
     set "CONFIG=Debug"
     set "CONFIG_ARG=-debug"
-) else if /i "%~1"=="-release" (
+    shift
+    goto parse_args
+)
+
+if /i "%~1"=="-release" (
     set "CONFIG=Release"
     set "CONFIG_ARG=-release"
-) else if not "%~1"=="" (
-    echo Usage: %~nx0 [-debug^|-release]
-    exit /b 1
+    shift
+    goto parse_args
 )
+
+rem cmd.exe splits "-game=name" into two args at the "=", so accept both
+rem the quoted form ("-game=name" as a single %1) and the unquoted one.
+if /i "%~1"=="-game" (
+    if "%~2"=="" (
+        echo Usage: %~nx0 [-debug^|-release] [-game=name]
+        exit /b 1
+    )
+    set "GAME=%~2"
+    shift
+    shift
+    goto parse_args
+)
+
+set "ARG=%~1"
+if /i "%ARG:~0,6%"=="-game=" (
+    set "GAME=%ARG:~6%"
+    shift
+    goto parse_args
+)
+
+echo Usage: %~nx0 [-debug^|-release] [-game=name]
+exit /b 1
+
+:args_done
 
 set "EXE_DIR=%~dp0build\%CONFIG%"
-set "EXE_PATH=%EXE_DIR%\dyro_game.exe"
+set "EXE_PATH=%EXE_DIR%\%GAME%.exe"
 
 if not exist "%EXE_PATH%" (
-    set "EXE_PATH=%EXE_DIR%\dyro_game_d.exe"
+    set "EXE_PATH=%EXE_DIR%\%GAME%_d.exe"
 )
 
 if not exist "%EXE_PATH%" (
-    echo Could not find dyro_game.exe in "%EXE_DIR%".
+    echo Could not find %GAME%.exe in "%EXE_DIR%".
     echo Run build.bat %CONFIG_ARG% first.
     exit /b 1
 )
